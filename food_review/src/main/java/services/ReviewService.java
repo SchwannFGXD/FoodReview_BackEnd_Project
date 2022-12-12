6595be79939dd3c07fade85efcc79d4ee4ec4a5e
Georgia Clarke <georgiaclarke@Georgias-MacBook-Pro.local>
2022-12-12 15:29:32 +0000

    review service created

---
+++ food_review/src/main/java/services/ReviewService.java
@@ -0,0 +1,41 @@
+package services;
+
+import model.FoodPlace;
+import model.Review;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import repositories.ReviewRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class ReviewService {
+
+    @Autowired
+    ReviewRepository reviewRepository;
+
+    public List<Review> displayAllReviews(){
+        return reviewRepository.findAll();
+    }
+
+    public Optional<Review> findReviewById(Long id){
+        return reviewRepository.findById(id);
+    }
+
+    public void addReview(Review review){
+        reviewRepository.save(review);
+    }
+
+    public void deleteReviewById(Long id){
+        reviewRepository.deleteById(id);
+    }
+
+    public void updateReview(Review review, Long id){
+        Review reviewToUpdate = reviewRepository.findById(id).get();
+        reviewToUpdate.setRating(review.getRating());
+        reviewToUpdate.setReviewText(review.getReviewText());
+        reviewToUpdate.setDate(review.getDate());
+        reviewRepository.save(reviewToUpdate);
+    }
+}

b44afc30c9f9a0917ee9263d7b9d58146ca6ac57
Shawn <shawnaugustine01@gmail.com>
2022-12-12 16:23:04 +0000

    updated user controller

---
+++ food_review/src/main/java/com/example/food_review/controllers/UserController.java
@@ -0,0 +1,57 @@
+package com.example.food_review.controllers;
+
+import model.FoodPlace;
+import model.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import services.FoodPlaceService;
+import services.UserService;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/User")
+public class UserController {
+
+    @Autowired
+    UserService userService;
+
+    @GetMapping
+    public ResponseEntity<List<User>> getAllUsers(){
+        List<User> user = userService.getAllUsers();
+        return new ResponseEntity<>(user, HttpStatus.OK);
+    }
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<User> getUserByID(@PathVariable long id){
+        Optional<User> user = userService.getUserById(id);
+        if(user.isPresent()){
+            return new ResponseEntity<>(user.get(), HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>(null, HttpStatus.NOT_FOUND);
+        }
+
+    }
+
+    @PostMapping
+    public ResponseEntity<User>  addNewUser(@RequestBody User user){
+        userService.addUser(user);
+        return new ResponseEntity<>(user, HttpStatus.CREATED);
+    }
+
+    @DeleteMapping(value = "/{id}")
+    public ResponseEntity<Long> deleteUserById(@PathVariable long id){
+        userService.removeUserByID(id);
+        return new ResponseEntity<>(id, HttpStatus.OK);
+    }
+
+    @PutMapping(value = "/{id}")
+    public  ResponseEntity<User> updateUser(@RequestBody User user, @PathVariable Long id){
+        userService.updateUser(user,id);
+        return new ResponseEntity<>(user, HttpStatus.OK);
+
+
+    }
+}

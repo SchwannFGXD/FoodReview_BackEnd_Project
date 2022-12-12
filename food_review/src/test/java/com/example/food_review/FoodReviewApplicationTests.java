2d5be3e4988ec12a9eccfa967c0f01dedb1a24e2
Shawn <shawnaugustine01@gmail.com>
2022-12-12 11:45:40 +0000

    folder

---
+++ food_review/src/test/java/com/example/food_review/FoodReviewApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.food_review;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FoodReviewApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

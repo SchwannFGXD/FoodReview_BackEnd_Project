6a8bc51a9ca9b030a5890620d5c0c61ea4526101
Shawn <shawnaugustine01@gmail.com>
2022-12-13 15:53:21 +0000

    tidied up dataloader

--- food_review/src/main/java/com/example/food_review/components/Dataloader.java
+++ food_review/src/main/java/com/example/food_review/components/Dataloader.java
@@ -22,15 +22,6 @@ import java.util.List;
 public class Dataloader implements ApplicationRunner {
 
     @Autowired
-    FoodPlaceService foodPlaceService;
-
-    @Autowired
-    UserService userService;
-
-    @Autowired
-    ReviewService reviewService;
-
-    @Autowired
     FoodPlaceRepository foodPlaceRepository;
 
     @Autowired
@@ -77,118 +68,25 @@ public class Dataloader implements ApplicationRunner {
 
 //
         Review review1 = new Review(3, "The food was decent.", LocalDate.of(2022,10,06), skewers_beers, Emma);
-      Review review2 = new Review(4.5, "The food was amazing.",LocalDate.of(2022,10, 12), chinChinLab, Shawn);
+        Review review2 = new Review(4.5, "The food was amazing.",LocalDate.of(2022,10, 12), chinChinLab, Shawn);
         Review review3 = new Review(1, "The establishment had rats falling from the ceiling.",LocalDate.of(2022,10,30),amigos, Salma);
         Review review4 = new Review(4, "Lost for words.",LocalDate.of(2022, 11, 01), silkRoad, Georgia);
         Review review5 = new Review(5, "Stunned.",LocalDate.of(2022,11,06), silkRoad, Zsolt);
-       Review review6 = new Review(2, "The noodles looked questionable.",LocalDate.of(2022,11,25),skewers_beers, Anna);
-       Review review7 = new Review(3.5, "Rude staff, I was ignored by the waiter.",LocalDate.of(2022,11,30), skewers_beers, Jenna);
+        Review review6 = new Review(2, "The noodles looked questionable.",LocalDate.of(2022,11,25),skewers_beers, Anna);
+        Review review7 = new Review(3.5, "Rude staff, I was ignored by the waiter.",LocalDate.of(2022,11,30), skewers_beers, Jenna);
         Review review8 = new Review(5, "The meal was exquisite.",LocalDate.of(2022,12,01), skewers_beers, Emma);
-       Review review9 = new Review(1.5, "My Chicken nugget looked like a cockroach.",LocalDate.of(2022,11,05), amigos, Emma);
+        Review review9 = new Review(1.5, "My Chicken nugget looked like a cockroach.",LocalDate.of(2022,11,05), amigos, Emma);
         Review review10 = new Review(4.5, "The food was rich in umami.",LocalDate.of(2022,11,25), tsujiri, Salma);
 
-//        reviewRepository.saveAll(List.of(review1, review2, review3, review4, review5, review6, review7, review8, review9, review10));
-        review1 = reviewRepository.save(review1);
-//        userService.addReview(review1, Emma);
-        review8 = reviewRepository.save(review8);
-//        userService.addReview(review8, Emma);
-        review9 = reviewRepository.save(review9);
-//        userService.addReview(review9, Emma);
-//
-//
-        review2 = reviewRepository.save(review2);
-//        userService.addReview(review2, Shawn);
-//
-        review3 = reviewRepository.save(review3);
-//        userService.addReview(review3, Salma);
-        review10 = reviewRepository.save(review10);
-//        userService.addReview(review10, Salma);
-//
-        review4 = reviewRepository.save(review4);
-//        userService.addReview(review4, Georgia);
-        review5 = reviewRepository.save(review5);
-//        userService.addReview(review5, Zsolt);
-       review6 = reviewRepository.save(review6);
-//        userService.addReview(review6, Anna);
-        review7 = reviewRepository.save(review7);
-//        userService.addReview(review7, Jenna);
-//
-//
-//
-  //      skewers_beers.addReview(review7);
-
-  //     skewers_beers.addReview(review1);
-  //     skewers_beers.addReview(review6);
-//        skewers_beers.addReview(review8);
-//
-    //    chinChinLab.addReview(review2);
-//
-//        silkRoad.addReview(review4);
-//        silkRoad.addReview(review5);
-//
-  //      amigos.addReview(review3);
-//        amigos.addReview(review9);
-//
-//        tsujiri.addReview(review10);
-
-
-//        foodPlaceService.addFoodPlace(tsujiri);
-//        foodPlaceRepository.save(tsujiri);
-//        foodPlaceService.addFoodPlace(amigos);
-//        foodPlaceRepository.save(amigos);
-//        foodPlaceService.addFoodPlace(silkRoad);
-//        foodPlaceRepository.save(silkRoad);
-//        foodPlaceService.addFoodPlace(chinChinLab);
-//        foodPlaceRepository.save(chinChinLab);
-//        foodPlaceService.addFoodPlace(skewers_beers);
-//        foodPlaceRepository.save(skewers_beers);
-//
-//        userService.addUser(Emma);
-//        userRepository.save(Emma);
-//        userService.addUser(Shawn);
-//        userRepository.save(Shawn);
-//        userService.addUser(Georgia);
-//        userRepository.save(Georgia);
-//        userService.addUser(Salma);
-//        userRepository.save(Salma);
-//        userService.addUser(Anna);
-//        userRepository.save(Anna);
-//        userService.addUser(Jenna);
-//        userRepository.save(Jenna);
-//        userService.addUser(Zsolt);
-//        userRepository.save(Zsolt);
-//        userService.addUser(Colin);
-//        userRepository.save(Colin);
-//        userService.addUser(Richard);
-//        userRepository.save(Richard);
-//        userService.addUser(Ed);
-//        userRepository.save(Ed);
-//
-//
-//        reviewService.addReview(review1);
-//        reviewRepository.save(review1);
-//        reviewService.addReview(review2);
-//        reviewRepository.save(review2);
-//        reviewService.addReview(review3);
-//        reviewRepository.save(review3);
-//        reviewService.addReview(review4);
-//        reviewRepository.save(review4);
-//        reviewService.addReview(review5);
-//        reviewRepository.save(review5);
-//        reviewService.addReview(review6);
-//        reviewRepository.save(review6);
-//        reviewService.addReview(review7);
-//        reviewRepository.save(review7);
-//        reviewService.addReview(review8);
-//        reviewRepository.save(review8);
-//        reviewService.addReview(review9);
-//        reviewRepository.save(review9);
-//        reviewService.addReview(review10);
-//        reviewRepository.save(review10);
-
-
+        reviewRepository.save(review1);
+        reviewRepository.save(review2);
+        reviewRepository.save(review3);
+        reviewRepository.save(review4);
+        reviewRepository.save(review5);
+        reviewRepository.save(review6);
+        reviewRepository.save(review7);
+        reviewRepository.save(review8);
+        reviewRepository.save(review9);
+        reviewRepository.save(review10);
     }
-
-
-
 }

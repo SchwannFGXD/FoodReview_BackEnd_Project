a786d8facc19095789f72851dc7fd7b12ade16ed
Shawn <shawnaugustine01@gmail.com>
2022-12-15 14:00:14 +0000

    commented code

--- food_review/src/main/java/com/example/food_review/controllers/UserController.java
+++ food_review/src/main/java/com/example/food_review/controllers/UserController.java
@@ -20,11 +20,13 @@ public class UserController {
     @Autowired
     UserService userService;
 
+    //get all users
     @GetMapping
     public ResponseEntity<List<User>> getAllUsers(){
         List<User> user = userService.getAllUsers();
         return new ResponseEntity<>(user, HttpStatus.OK);
     }
+
 
     @GetMapping(value = "/username")
     public ResponseEntity<List<User>> getAllUsersAndFilters(
@@ -37,7 +39,7 @@ public class UserController {
         return new ResponseEntity<>(null, HttpStatus.NO_CONTENT);
     }
 
-
+    //get user by id
     @GetMapping(value = "/{id}")
     public ResponseEntity<User> getUserByID(@PathVariable long id){
         Optional<User> user = userService.getUserById(id);
@@ -49,7 +51,7 @@ public class UserController {
 
     }
 
-
+    //get all reviews from a user
     @GetMapping(value = "/{id}/reviews")
     public ResponseEntity<List<Review>> getAllReviewsFromUserById(@PathVariable long id) {
         Optional <User> user = userService.getUserById(id);
@@ -57,24 +59,29 @@ public class UserController {
         return new ResponseEntity<>(reviews, HttpStatus.OK);
     }
 
+    //get all foodplaces a user has reviewed
     @GetMapping(value = "/{id}/foodplace")
     public ResponseEntity<List<FoodPlace>> getFoodPlacesFromUser(@PathVariable long id) {
         Optional<User> user = userService.getUserById(id);
         List<FoodPlace> foodPlaces = userService.getAllFoodPlacesFromUser(user.get());
         return new ResponseEntity<>(foodPlaces, HttpStatus.OK);
     }
+
+    //add user
     @PostMapping
     public ResponseEntity<User>  addNewUser(@RequestBody User user){
         userService.addUser(user);
         return new ResponseEntity<>(user, HttpStatus.CREATED);
     }
 
+    //add delete user
     @DeleteMapping(value = "/{id}")
     public ResponseEntity<Long> deleteUserById(@PathVariable long id){
         userService.removeUserByID(id);
         return new ResponseEntity<>(id, HttpStatus.OK);
     }
 
+    //update user
     @PutMapping(value = "/{id}")
     public  ResponseEntity<User> updateUser(@RequestBody User user, @PathVariable Long id){
         userService.updateUser(user,id);
